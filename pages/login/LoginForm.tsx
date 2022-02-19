be499413aca8a7d6b37bc29076588fe7eb91fe19
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2022-02-19 14:48:22 +0300

    login page ui

--- pages/login/LoginForm.tsx
+++ pages/login/LoginForm.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Form, Input, Button, Card } from 'antd';
 import PropTypes from 'prop-types';
+import styles from './Login.module.css';
+import { LeftSquareFilled } from '@ant-design/icons';
+import { UserOutlined } from '@ant-design/icons';
+import { useRouter } from 'next/router';
 
-function LoginForm({ handleLogin }) {
+function LoginForm({ handleLogin, cardStyles }) {
   const onFinish = async (values: any) => {
     await handleLogin(values);
   };
+  const router = useRouter();
 
   const onFinishFailed = (errorInfo: any) => {
     return errorInfo;
@@ -16,9 +21,21 @@ function LoginForm({ handleLogin }) {
       style={{
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center'
+        alignItems: 'center',
+        ...cardStyles
       }}
     >
+      <div className={styles.registerIconWrapper}>
+        <div className={styles.registerIcon}>
+          <LeftSquareFilled
+            className={styles.registerIconComponent}
+            onClick={() => router.push('/register')}
+          />
+          <div className={styles.registerLabel}>
+            <span className={styles.registerLabelSpan}>Register</span>
+          </div>
+        </div>
+      </div>
       <Form
         name="basic"
         wrapperCol={{ span: 16 }}
@@ -35,22 +52,26 @@ function LoginForm({ handleLogin }) {
         <Form.Item
           wrapperCol={{ span: 32 }}
           name="username"
+          style={{ width: '300px' }}
           label="Username"
+          className={styles.formLabel}
           rules={[{ required: true, Message: 'Please input your username!' } as any]}
         >
-          <Input />
+          <Input suffix={<UserOutlined />} />
         </Form.Item>
 
         <Form.Item
           wrapperCol={{ span: 32 }}
           name="password"
           label="Password"
+          style={{ width: '300px' }}
+          className={styles.formLabel}
           rules={[{ required: true, Message: 'Please input your password!' } as any]}
         >
           <Input.Password />
         </Form.Item>
 
-        <Form.Item wrapperCol={{ span: 24 }}>
+        <Form.Item wrapperCol={{ span: 32 }} className={styles.loginBtn}>
           <Button type="primary" htmlType="submit">
             Login
           </Button>
@@ -61,6 +82,12 @@ function LoginForm({ handleLogin }) {
 }
 
 LoginForm.propTypes = {
-  handleLogin: PropTypes.func
+  handleLogin: PropTypes.func,
+  cardStyles: PropTypes.object
 };
+LoginForm.defaultProps = {
+  handleLogin: () => {},
+  cardStyles: {}
+};
+
 export default LoginForm;
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,7 +6,14 @@ import { UserContext } from '../../context/UserContext';
 import { useRouter } from 'next/router';
 import LoginForm from './LoginForm';
 import Cookies from 'js-cookie';
-
+import Image from 'next/image';
+import Ellipse from './assets/ellipse.png';
+import Layer1 from './assets/Layer1.png';
+import Layer2 from './assets/Layer2.png';
+import Plus from './assets/plus.png';
+import Triangle from './assets/triangle.png';
+import { Typography } from 'antd';
+const { Title } = Typography;
 function Index() {
   const initialState = {
     loading: false,
@@ -52,18 +59,95 @@ function Index() {
   const { setUser } = useContext(UserContext);
 
   return (
-    <Layout
-      style={{
-        width: '%100',
-        height: '100vh',
-        alignContent: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <Spin spinning={state.loading} delay={400}>
-        <LoginForm handleLogin={handleLogin} />
-      </Spin>
-    </Layout>
+    <Spin spinning={state.loading}>
+      <Layout
+        style={{
+          width: '%100',
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'row'
+        }}>
+        <div
+          style={{
+            width: '100vw',
+            height: '100%',
+            backgroundColor: '#f64747',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'relative',
+            boxShadow: '1px -5px 300px #000000'
+          }}
+          className="wrapper">
+          <div className="content" style={{ zIndex: '5' }}>
+            <Title style={{ color: 'white' }} level={1}>
+              Welcome back.
+            </Title>
+
+            <Typography.Text style={{ color: 'white', fontSize: '20px', fontWeight: 'lighter' }}>
+              You can sign in to access with your existing account.
+            </Typography.Text>
+          </div>
+          <div className="images" style={{ zIndex: '1' }}>
+            <div
+              style={{ position: 'absolute', left: '10%', top: '10%', width: '4%', zIndex: '9' }}>
+              <Image src={Plus} alt="Layer1" />
+            </div>
+            <div
+              style={{ position: 'absolute', left: '50%', top: '40%', width: '3%', zIndex: '9' }}>
+              <Image src={Plus} alt="Layer1" />
+            </div>
+            <div
+              style={{ position: 'absolute', left: '30%', top: '15%', width: '3%', zIndex: '9' }}>
+              <Image src={Ellipse} alt="Layer1" />
+            </div>
+            <div
+              style={{ position: 'absolute', left: '80%', top: '90%', width: '5%', zIndex: '9' }}>
+              <Image src={Ellipse} alt="Layer1" />
+            </div>
+
+            <div
+              style={{ position: 'absolute', left: '28%', top: '67%', width: '4%', zIndex: '9' }}>
+              <Image src={Triangle} alt="Layer1" />
+            </div>
+            <div
+              style={{ position: 'absolute', left: '68%', top: '48%', width: '2%', zIndex: '9' }}>
+              <Image src={Triangle} alt="Layer1" />
+            </div>
+            <div style={{ position: 'absolute', left: '0', bottom: '0', width: '80%' }}>
+              <Image src={Layer1} objectPosition="left bottom" />
+            </div>
+            <div style={{ position: 'absolute', left: '0', bottom: '0', width: '80%' }}>
+              <Image src={Layer2} objectPosition="left bottom" />
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="right-side"
+          style={{
+            width: '100%',
+            height: '100%',
+            padding: '20px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'relative'
+          }}>
+          <LoginForm
+            handleLogin={handleLogin}
+            cardStyles={{
+              width: '70%',
+              height: '70%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          />
+        </div>
+      </Layout>
+    </Spin>
   );
 }
 
