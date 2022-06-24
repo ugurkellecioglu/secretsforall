a0f098290a94c3f2525646344e53ecd975addfeb
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2022-06-24 21:31:34 +0300

    top menu for mobile

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -3,12 +3,14 @@ import React, { useContext } from 'react';
 import { HeaderContext } from '../context/HeaderContext';
 import Header from './Header/Header';
 import Sidebar from './Sidebar/Sidebar';
+import TopMenu from './TopMenu';
 type Props = { children: React.ReactNode };
 const Overlay: React.FC<Props> = ({ children }) => {
   const { collapsed } = useContext(HeaderContext);
   return (
     <div style={{ overflowX: 'hidden', height: '100%', backgroundColor: '#f0f2f5' }}>
       <Header />
+      <TopMenu collapsed={collapsed} />
       <Layout style={{ height: '90vh', width: '100%' }}>
         <Sidebar collapsed={collapsed} />
         <Layout.Content>
---
+++ components/TopMenu/index.tsx
@@ -0,0 +1,33 @@
+import { AppstoreTwoTone, StarTwoTone } from '@ant-design/icons';
+import { Menu } from 'antd';
+import Link from 'next/link';
+import React, { useEffect, useRef } from 'react';
+import styles from './TopMenu.module.scss';
+const TopMenu: React.FC<{ collapsed: boolean }> = ({ collapsed }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      if (!collapsed) {
+        ref.current.classList.add(styles.active);
+      } else if (ref.current.classList.contains(styles.active)) {
+        ref.current.classList.remove(styles.active);
+      }
+    }
+  }, [collapsed]);
+
+  return (
+    <div className={`${styles.wrapper} `} ref={ref}>
+      <Menu mode="inline" defaultSelectedKeys={['1']}>
+        <Menu.Item key="1" icon={<AppstoreTwoTone />}>
+          <Link href="/dashboard"> Dashboard</Link>
+        </Menu.Item>
+        <Menu.Item key="2" icon={<StarTwoTone />}>
+          <Link href="/random-secret">Random Secret</Link>
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
+
+export default TopMenu;
