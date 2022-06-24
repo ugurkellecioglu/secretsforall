0f74652b0d94a4cfa47a0a1cf46b3d92c01c6b9e
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2022-06-24 21:40:20 +0300

    detect device hook

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -4,15 +4,17 @@ import { HeaderContext } from '../context/HeaderContext';
 import Header from './Header/Header';
 import Sidebar from './Sidebar/Sidebar';
 import TopMenu from './TopMenu';
+import useDeviceDetect from '../helpers/useDeviceDetect';
 type Props = { children: React.ReactNode };
 const Overlay: React.FC<Props> = ({ children }) => {
   const { collapsed } = useContext(HeaderContext);
+  const { isMobile } = useDeviceDetect();
   return (
     <div style={{ overflowX: 'hidden', height: '100%', backgroundColor: '#f0f2f5' }}>
       <Header />
-      <TopMenu collapsed={collapsed} />
+      {isMobile && <TopMenu collapsed={collapsed} />}
       <Layout style={{ height: '90vh', width: '100%' }}>
-        <Sidebar collapsed={collapsed} />
+        {!isMobile && <Sidebar collapsed={collapsed} />}
         <Layout.Content>
           <div style={{ marginTop: '30px' }}>{children}</div>
         </Layout.Content>
