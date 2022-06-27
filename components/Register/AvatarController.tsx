dafde2046c76811be0243119011b1dde19c4db40
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2022-06-27 16:01:50 +0300

    register shuffle icon

--- components/Register/AvatarController.tsx
+++ components/Register/AvatarController.tsx
@@ -1,12 +1,16 @@
 import { Button, Col, Typography } from 'antd';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { RedoOutlined } from '@ant-design/icons';
+import styles from './style.module.scss';
 
-function AvatarController({ handleAvatarChange, styles }) {
+function AvatarController({ handleAvatarChange }) {
   return (
     <>
       <Col span={24} style={{ textAlign: 'center' }}>
-        <Button onClick={() => handleAvatarChange()} className={styles && styles.icon} />
+        <div className={styles.icon}>
+          <RedoOutlined onClick={() => handleAvatarChange()} />
+        </div>
       </Col>
       <Col span={24} style={{ textAlign: 'center' }}>
         <Typography.Text>Roll the Dice!</Typography.Text>
